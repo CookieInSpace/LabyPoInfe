18537e2683c8c44638654ba13f7601814037564e
CookieInSpace <loki.cheese@gmail.com>
2016-09-29 20:27:46 +0500

    Это первый коммит

---
+++ src/src/Laba1/Main.java
@@ -0,0 +1,14 @@
+package Laba1;
+
+/**
+ * Created by Домашний on 29.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world!");
+        }
+
+    }
+}
---
+++ src/src/Laba2/Main.java
@@ -0,0 +1,12 @@
+package Laba2;
+
+/**
+ * Created by Домашний on 29.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("А это лаба 2");
+        }
+    }
+}
